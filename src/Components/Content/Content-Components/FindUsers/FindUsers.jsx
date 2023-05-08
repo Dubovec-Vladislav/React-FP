cf031f6c217601d7b3506e33b8aba5618723fa3f
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-05-08 08:22:20 +0300

    Adding a constructor in class component

--- src/Components/Content/Content-Components/FindUsers/FindUsers.jsx
+++ src/Components/Content/Content-Components/FindUsers/FindUsers.jsx
@@ -4,13 +4,13 @@ import userPhoto from '../../../../assets/img/user.png'
 // import './React.css'
 
 class FindUsers extends React.Component {
-    getUsers = () => {
-        if (this.props.usersList.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items);
-            });
-        };
-    };
+
+    constructor(props) {
+        super(props);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    }
 
     render() {
         let usersItems = this.props.usersList.map(user => {
