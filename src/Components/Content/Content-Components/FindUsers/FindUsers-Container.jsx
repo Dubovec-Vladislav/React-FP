8000b29e3f4c2c0fe2278901598fb362531cfc80
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-05-09 10:30:11 +0300

    Return to clear function component

--- src/Components/Content/Content-Components/FindUsers/FindUsers-Container.jsx
+++ src/Components/Content/Content-Components/FindUsers/FindUsers-Container.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 // import './React.css'
-import { addFriendActionCreator, delFriendActionCreator, setUsersActionCreator, updateCurrentPageActionCreator, setTotalUsersCountActionCreator } from '../../../../redux/find-user-reducer';
-import FindUsersApiContainer from './FindUsersApi-Container'
+import axios from 'axios'
+import FindUsers from './FindUsers'
+import { addFriendActionCreator, delFriendActionCreator, setUsersActionCreator, updateCurrentPageActionCreator, setTotalUsersCountActionCreator } from '../../../../redux/find-user-reducer'
 import { connect } from 'react-redux'
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            if (response.data.totalCount < 100) {
+                this.props.setTotalUsersCount(response.data.totalCount);
+            } else {
+                this.props.setTotalUsersCount(100);
+            }
+        });
+    };
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    };
+
+    render() {
+        return <FindUsers totalUsersCount={this.props.totalUsersCount} pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage} usersList={this.props.usersList}
+            onPageChanged={this.onPageChanged} delFriend={this.props.delFriend} addFriend={this.props.addFriend} />;
+    };
+};
 
 function mapStateToProps(state) {
     return {
@@ -23,6 +50,6 @@ function mapDispatchToProps(dispatch) {
     };
 };
 
-const FindUsersContainer = connect(mapStateToProps, mapDispatchToProps)(FindUsersApiContainer);
+const FindUsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
 
 export default FindUsersContainer;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,7 +1,7 @@
 import { combineReducers, createStore } from 'redux'
-import headerReducer from './header-reducer';
-import dialogsReducer from './dialogs-reducer';
-import findUsersReducer from './find-user-reducer';
+import headerReducer from './header-reducer'
+import dialogsReducer from './dialogs-reducer'
+import findUsersReducer from './find-user-reducer'
 
 let reducers = combineReducers({
     headerPage: headerReducer,
