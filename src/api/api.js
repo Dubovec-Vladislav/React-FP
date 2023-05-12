48557810f9c87beaa5ab3ff264c4729ad7a99202
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-05-12 08:48:56 +0300

    Creating instance in api

--- src/api/api.js
+++ src/api/api.js
@@ -1,35 +1,28 @@
 import axios from "axios"
 
+const baseUrl = "https://social-network.samuraijs.com/api/1.0";
+
+const instance = axios.create({
+    withCredentials: true,
+    headers: { "API-KEY": "5b281dfb-d275-44f3-9d8c-207a490b6d13" },
+});
+
 export function getUsers(currentPage = 1, pageSize = 5) {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-        { withCredentials: true, }
-    ).then(response => response.data);
+    return instance.get(baseUrl + `/users?page=${currentPage}&count=${pageSize}`).then(response => response.data);
 };
 
 export function authMe() {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`,
-        { withCredentials: true }
-    ).then(response => response.data);
+    return instance.get(baseUrl + `/auth/me`).then(response => response.data);
 };
 
 export function getUserProfile(userId) {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then(response => response.data);
+    return instance.get(baseUrl + `/profile/${userId}`).then(response => response.data);
 };
 
 export function delFriend(userId) {
-    return axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`,
-        {
-            withCredentials: true,
-            headers: { "API-KEY": "5b281dfb-d275-44f3-9d8c-207a490b6d13" },
-        }
-    ).then(response => response.data);
+    return instance.delete(baseUrl + `/follow/${userId}`).then(response => response.data);
 };
 
 export function addFriend(userId) {
-    return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, {},
-        {
-            withCredentials: true,
-            headers: { "API-KEY": "5b281dfb-d275-44f3-9d8c-207a490b6d13" },
-        }
-    ).then(response => response.data);
+    return instance.post(baseUrl + `/follow/${userId}`).then(response => response.data);
 };
