f712d469508b38f8eb2561f566e39478a4bc3a8d
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-05-18 18:47:24 +0300

    Creating a status check in ProfileStatus

--- src/Components/Content/Content-Components/Profile/ProfileStatus.jsx
+++ src/Components/Content/Content-Components/Profile/ProfileStatus.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 // import './React.css'
 
 class ProfileStatus extends React.Component {
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status,
+            });
+        }
+    };
 
     state = {
         editMode: false,
