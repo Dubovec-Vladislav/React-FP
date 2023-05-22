740bf9a1ecefb07bffbffe945e3733ec8d474f92
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-05-22 08:39:46 +0300

    Changing the class component of the status to a functional

--- src/Components/Content/Content-Components/Profile/ProfileStatus.jsx
+++ src/Components/Content/Content-Components/Profile/ProfileStatus.jsx
@@ -1,45 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 // import './React.css'
 
-class ProfileStatus extends React.Component {
-    componentDidUpdate(prevProps, prevState) {
-        if (prevProps.status !== this.props.status) {
-            this.setState({
-                status: this.props.status,
-            });
+
+const ProfileStatus = (props) => {
+    const [editMode, setEditMode] = useState(false);
+    const [status, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        if (status !== props.status) {
+            setStatus(props.status);
         }
+    }, [props.status]);
+
+    const activateEditMode = () => {
+        setEditMode(true);
     };
 
-    state = {
-        editMode: false,
-        status: this.props.status,
+    const deactivateEditMode = () => {
+        setEditMode(false);
+        props.updateUserStatus(status);
     };
 
-    activateEditMode = () => {
-        this.setState({ editMode: true });
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
     };
 
-    deactivateEditMode = () => {
-        this.setState({ editMode: false });
-        this.props.updateUserStatus(this.state.status);
-    };
-
-    onStatusChange = (e) => {
-        this.setState({ status: e.currentTarget.value });
-    };
-
-    render() {
-        return (
-            <div className="status__block">
-                {!this.state.editMode
-                    ?
-                    <div onDoubleClick={this.activateEditMode}>{this.props.status || "-----"}</div>
-                    :
-                    <textarea onChange={this.onStatusChange} onBlur={this.deactivateEditMode} value={this.state.status} type="text" autoFocus rows="5" cols="100" />
-                }
-            </div>
-        );
-    }
+    return (
+        <div className="status__block">
+            {!editMode ? (
+                <div onDoubleClick={activateEditMode}>{props.status || '-----'}</div>
+            ) : (
+                <textarea
+                    onChange={onStatusChange}
+                    onBlur={deactivateEditMode}
+                    value={status}
+                    type="text"
+                    autoFocus
+                    rows="5"
+                    cols="100"
+                />
+            )}
+        </div>
+    );
 };
 
 export default ProfileStatus;
+
+// class ProfileStatus extends React.Component {
+//     componentDidUpdate(prevProps, prevState) {
+//         if (prevProps.status !== this.props.status) {
+//             this.setState({
+//                 status: this.props.status,
+//             });
+//         }
+//     };
+
+//     state = {
+//         editMode: false,
+//         status: this.props.status,
+//     };
+
+//     activateEditMode = () => {
+//         this.setState({ editMode: true });
+//     };
+
+//     deactivateEditMode = () => {
+//         this.setState({ editMode: false });
+//         this.props.updateUserStatus(this.state.status);
+//     };
+
+//     onStatusChange = (e) => {
+//         this.setState({ status: e.currentTarget.value });
+//     };
+
+//     render() {
+//         return (
+//             <div className="status__block">
+//                 {!this.state.editMode
+//                     ?
+//                     <div onDoubleClick={this.activateEditMode}>{this.props.status || "-----"}</div>
+//                     :
+//                     <textarea onChange={this.onStatusChange} onBlur={this.deactivateEditMode} value={this.state.status} type="text" autoFocus rows="5" cols="100" />
+//                 }
+//             </div>
+//         );
+//     }
+// };
+
+// export default ProfileStatus;
